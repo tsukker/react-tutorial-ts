baeadc8777493150bcb9182ff49ab01162d8c40f
tsukker <tsukker@users.noreply.github.com>
2021-09-10 18:00:33 +0900

    Update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders `Next`", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Next/i);
   expect(linkElement).toBeInTheDocument();
 });
