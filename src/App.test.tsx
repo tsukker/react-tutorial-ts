bc672e44332990ea0feede6ea567732dbef0f5c5
tsukker <tsukker@users.noreply.github.com>
2021-09-13 23:49:20 +0900

    Add a test: "move #1"

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders `Next`", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Next/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Next/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test("renders `Go to move #1`", () => {
+  render(<App />);
+  const squares = document.getElementsByClassName("square");
+  expect(squares.length).toEqual(9);
+  act(() => {
+    squares[0].dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  const textElement = screen.getByText(/Go to move #1/i);
+  expect(textElement).toBeInTheDocument();
 });
